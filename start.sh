bd04a3e28fe12ce9897ee983023a7de5a4889498
Anna Rooks <a.Git@ironflurry.com>
2019-11-27 22:43:38 -0500

    fixed script to start and stop all processes in background

--- start.sh
+++ start.sh
@@ -1,4 +1,10 @@
 #!/bin/sh
-python3 log/notify.py
-python3 backend/app.py
-front/manage.py runsslserver 0.0.0.0:8000
+cd ..
+python3 RAAD/log/notify.py &
+python3 RAAD/backend/app.py &
+python3 RAAD/front/manage.py runsslserver 0.0.0.0:8000 &
+trap "pkill -f RAAD"
+while true
+do
+    sleep 100
+done
