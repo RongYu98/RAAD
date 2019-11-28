a1ec0c04e9187cf41893082ecdb3c0f617f2594d
Anna Rooks <a.Git@ironflurry.com>
2019-11-27 22:44:09 -0500

    added int

--- start.sh
+++ start.sh
@@ -3,7 +3,7 @@ cd ..
 python3 RAAD/log/notify.py &
 python3 RAAD/backend/app.py &
 python3 RAAD/front/manage.py runsslserver 0.0.0.0:8000 &
-trap "pkill -f RAAD"
+trap "pkill -f RAAD" INT
 while true
 do
     sleep 100
