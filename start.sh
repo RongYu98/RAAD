ab87f23fc0cac61a0f961365b64e949db955c5ff
Amy Yu <amyyu@cs.stonybrook.edu>
2019-12-02 03:20:17 +0000

    fix: start script now exits properly

--- start.sh
+++ start.sh
@@ -3,7 +3,8 @@ cd ..
 python3 RAAD/logs/notify.py &
 python3 RAAD/backend/app.py &
 python3 RAAD/front/manage.py runsslserver 0.0.0.0:8000 &
-trap "pkill -f RAAD" INT
+trap "pkill -f RAAD; sleep 2; kill $$" INT
+
 while true
 do
     sleep 100
